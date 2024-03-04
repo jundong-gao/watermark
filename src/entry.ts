b036d5ddad9e9eb4bd3cee8d2a95d60af0e2d24c
高俊东 <1643549042@qq.com>
2024-03-04 18:01:08 +0800

    🐞 fix: options调试

--- src/entry.ts
+++ src/entry.ts
@@ -1,5 +1,7 @@
+import { IMarkConfig } from "gao-watermark";
 import waterMark from "./components/water-mark.vue";
-const install = (app: any) => {
+const install = (app: any, options: IMarkConfig) => {
+  console.log('接收到的默认参数为::::::::::::::::', options)
   app.component('WaterMark', waterMark);
 }
 
@@ -7,6 +9,7 @@ let component = {
   waterMark,
   install
 }
+
 
 export {
   waterMark,
